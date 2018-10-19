0952a007b8f2fc3ef771c4510968324d94faf65c
Murtaza@Piplod <murtaza1926@gmail.com>
2018-10-19 10:41:38 +0530

    fix typo

--- superior_tissot.mk
+++ superior_tissot.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/superior/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := superio_tissot
+PRODUCT_NAME := superior_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
